b98b017ca8297983105165fc56e292fcfe3db081
Grant Birkinbine <grantbirki@github.com>
2022-04-28 17:10:25 -0400

    base working

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import * as core from '@actions/core'
+import {context} from '@actions/github'
 // import {wait} from './wait'
-// import * as GitHub from '@actions/github'
 
 async function run(): Promise<void> {
   try {
     const trigger: string = core.getInput('trigger')
     const reaction: string = core.getInput('reaction')
     const token: string = core.getInput('github-token', {required: true})
+    const body: string = context?.payload?.comment?.body
 
-    core.info(`trigger: ${trigger}`)
-    core.info(`reaction: ${reaction}`)
+    // core.info(`context: ${JSON.stringify(context)}`)
 
     core.setOutput('triggered', 'true')
+    core.setOutput('comment_body', body)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
