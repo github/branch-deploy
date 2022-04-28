ea7ed53fa99fcae4276e1062a19d67b2498cac52
Grant Birkinbine <grantbirki@github.com>
2022-04-28 17:35:44 -0400

    use trigger checks

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import * as core from '@actions/core'
 import {context} from '@actions/github'
-// import {wait} from './wait'
+import {triggerCheck} from './trigger-check'
 
 async function run(): Promise<void> {
   try {
     const trigger: string = core.getInput('trigger')
     const reaction: string = core.getInput('reaction')
+    const prefixOnly: boolean = core.getInput('prefix_only') === 'true'
     const token: string = core.getInput('github-token', {required: true})
     const body: string = context?.payload?.comment?.body
 
-    // core.info(`context: ${JSON.stringify(context)}`)
+    const triggerResult = await triggerCheck(prefixOnly, body, trigger)
 
-    core.setOutput('triggered', 'true')
+    core.info(`prefixOnly: ${prefixOnly}`)
+    core.info(`triggerResult: ${triggerResult}`)
     core.setOutput('comment_body', body)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
@@ -19,3 +21,5 @@ async function run(): Promise<void> {
 }
 
 run()
+
+// core.info(`context: ${JSON.stringify(context)}`)
