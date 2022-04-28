877098b78d6ad529b346d55552ac6c0cd7e1c7a6
Grant Birkinbine <grantbirki@github.com>
2022-04-28 17:35:34 -0400

    add trigger checks

---
+++ src/trigger-check.ts
@@ -0,0 +1,22 @@
+import * as core from '@actions/core'
+
+// A simple function that checks the body of the message against the trigger
+// Returns true if a message trips the trigger
+// Returns false if a message does not trip the trigger
+export async function triggerCheck(
+  prefixOnly: boolean,
+  body: string,
+  trigger: string
+): Promise<boolean> {
+  return new Promise(resolve => {
+    // If the the trigger is not activated, set the output to false and return with false
+    if ((prefixOnly && !body.startsWith(trigger)) || !body.includes(trigger)) {
+      core.setOutput('triggered', 'false')
+      return resolve(false)
+    }
+
+    // If the trigger is activated, set the output to true and return with true
+    core.setOutput('triggered', 'true')
+    return resolve(true)
+  })
+}
