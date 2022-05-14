e03dfb4166f4362b6aae68535ad7114723b456a2
Grant Birkinbine <grantbirki@github.com>
2022-05-14 10:40:08 -0600

    improved tests

--- __tests__/functions/trigger-check.test.js
+++ __tests__/functions/trigger-check.test.js
@@ -1,29 +1,42 @@
 import {triggerCheck} from '../../src/functions/trigger-check'
+import * as core from '@actions/core'
 
 test('checks a message and finds a prefix trigger', async () => {
   const prefixOnly = true
   const body = '.deploy'
   const trigger = '.deploy'
+  const setOutputMock = jest.spyOn(core, 'setOutput')
   expect(await triggerCheck(prefixOnly, body, trigger)).toBe(true)
+  expect(setOutputMock).toHaveBeenCalledWith('triggered', 'true')
+  expect(setOutputMock).toHaveBeenCalledWith('comment_body', '.deploy')
 })
 
 test('checks a message and does not find prefix trigger', async () => {
   const prefixOnly = true
   const body = '.bad'
   const trigger = '.deploy'
+  const setOutputMock = jest.spyOn(core, 'setOutput')
   expect(await triggerCheck(prefixOnly, body, trigger)).toBe(false)
+  expect(setOutputMock).toHaveBeenCalledWith('triggered', 'false')
+  expect(setOutputMock).toHaveBeenCalledWith('comment_body', '.bad')
 })
 
 test('checks a message and finds a global trigger', async () => {
   const prefixOnly = false
   const body = 'I want to .deploy'
   const trigger = '.deploy'
+  const setOutputMock = jest.spyOn(core, 'setOutput')
   expect(await triggerCheck(prefixOnly, body, trigger)).toBe(true)
+  expect(setOutputMock).toHaveBeenCalledWith('triggered', 'true')
+  expect(setOutputMock).toHaveBeenCalledWith('comment_body', 'I want to .deploy')
 })
 
 test('checks a message and does not find global trigger', async () => {
   const prefixOnly = false
   const body = 'I want to .ping a website'
   const trigger = '.deploy'
+  const setOutputMock = jest.spyOn(core, 'setOutput')
   expect(await triggerCheck(prefixOnly, body, trigger)).toBe(false)
+  expect(setOutputMock).toHaveBeenCalledWith('triggered', 'false')
+  expect(setOutputMock).toHaveBeenCalledWith('comment_body', 'I want to .ping a website')
 })
