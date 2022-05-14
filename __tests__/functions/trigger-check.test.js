c44b000ed69454def9168233867d164228257955
Grant Birkinbine <grantbirki@github.com>
2022-05-14 10:17:51 -0600

    trigger check test coverage

--- __tests__/functions/trigger-check.test.js
+++ __tests__/functions/trigger-check.test.js
@@ -6,3 +6,24 @@ test('checks a message and finds a prefix trigger', async () => {
   const trigger = '.deploy'
   expect(await triggerCheck(prefixOnly, body, trigger)).toBe(true)
 })
+
+test('checks a message and does not find prefix trigger', async () => {
+  const prefixOnly = true
+  const body = '.bad'
+  const trigger = '.deploy'
+  expect(await triggerCheck(prefixOnly, body, trigger)).toBe(false)
+})
+
+test('checks a message and finds a global trigger', async () => {
+  const prefixOnly = false
+  const body = 'I want to .deploy'
+  const trigger = '.deploy'
+  expect(await triggerCheck(prefixOnly, body, trigger)).toBe(true)
+})
+
+test('checks a message and does not find global trigger', async () => {
+  const prefixOnly = false
+  const body = 'I want to .ping a website'
+  const trigger = '.deploy'
+  expect(await triggerCheck(prefixOnly, body, trigger)).toBe(false)
+})
