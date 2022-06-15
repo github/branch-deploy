de1170e4558d85ebe7f1040c685f335127e6a784
grantbirki <grantbirki@github.com>
2022-06-15 13:45:35 -0600

    test 500 api error

--- __tests__/functions/unlock.test.js
+++ __tests__/functions/unlock.test.js
@@ -23,15 +23,28 @@ const octokit = {
     rest: {
         git: {
             deleteRef: jest.fn().mockReturnValue({ status: 204 })
-        },
-        issues: {
-            createComment: jest.fn().mockReturnValue({ data: {} })
         }
     }
 }
 
 test('successfully releases a deployment lock with the unlock function', async () => {
     expect(await unlock(octokit, context, 123)).toBe(true)
+    expect(octokit.rest.git.deleteRef).toHaveBeenCalledWith({
+        owner: 'corp',
+        repo: 'test',
+        ref: 'heads/branch-deploy-lock'
+    })
+})
+
+test('fails to release a deployment lock due to a bad HTTP code from the GitHub API', async () => {
+    const badHttpOctokitMock = {
+        rest: {
+            git: {
+                deleteRef: jest.fn().mockReturnValue({ status: 500 })
+            }
+        }
+    }
+    expect(await unlock(badHttpOctokitMock, context, 123)).toBe(false)
     expect(octokit.rest.git.deleteRef).toHaveBeenCalledWith({
         owner: 'corp',
         repo: 'test',
