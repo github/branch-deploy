a2605f780bca6416acc2d07645c8549e819f1f98
grantbirki <grantbirki@github.com>
2022-06-15 14:10:27 -0600

    100% test coverage for unlock.js

--- __tests__/functions/unlock.test.js
+++ __tests__/functions/unlock.test.js
@@ -2,6 +2,13 @@ import * as core from '@actions/core'
 import { unlock } from '../../src/functions/unlock'
 import * as actionStatus from '../../src/functions/action-status'
 
+class NotFoundError extends Error {
+    constructor(message) {
+        super(message);
+        this.status = 422;
+    }
+}
+
 beforeEach(() => {
     jest.spyOn(actionStatus, 'actionStatus').mockImplementation(() => {
         return undefined
@@ -51,3 +58,34 @@ test('fails to release a deployment lock due to a bad HTTP code from the GitHub 
         ref: 'heads/branch-deploy-lock'
     })
 })
+
+test('Does not find a deployment lock branch so it lets the user know', async () => {
+    const actionStatusSpy = jest.spyOn(actionStatus, 'actionStatus').mockImplementation(() => {
+        return undefined
+    })
+    const noBranchOctokitMock = {
+        rest: {
+            git: {
+                deleteRef: jest.fn().mockRejectedValue(new NotFoundError('Reference does not exist'))
+            }
+        }
+    }
+    expect(await unlock(noBranchOctokitMock, context, 123)).toBe(true)
+    expect(actionStatusSpy).toHaveBeenCalledWith(context, noBranchOctokitMock, 123, '🔓 There is currently no deployment lock set', true, true)
+})
+
+test('throws an error if an unhandled exception occurs', async () => {
+    const errorOctokitMock = {
+        rest: {
+            git: {
+                deleteRef: jest.fn().mockRejectedValue(new Error('oh no'))
+            }
+        }
+    }
+    try {
+        await unlock(errorOctokitMock, context, 123)
+    } catch (e) {
+        expect(e.message).toBe('Error: oh no')
+    }
+})
+
