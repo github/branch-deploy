90b6f11d8b3a5c79cb1b88cf145d919fe4bea368
grantbirki <grantbirki@github.com>
2022-06-16 13:57:04 -0600

    update lock tests

--- __tests__/functions/lock.test.js
+++ __tests__/functions/lock.test.js
@@ -140,6 +140,82 @@ test('Determines that another user has the lock and exits - during a direct lock
   )
 })
 
+test('Request detailsOnly on the lock file and gets lock file data successfully', async () => {
+  const octokit = {
+    rest: {
+      repos: {
+        getBranch: jest
+          .fn()
+          .mockReturnValueOnce({data: {commit: {sha: 'abc123'}}}),
+        get: jest.fn().mockReturnValue({data: {default_branch: 'main'}}),
+        getContent: jest
+          .fn()
+          .mockReturnValue({data: {content: lockBase64Octocat}})
+      }
+    }
+  }
+  expect(await lock(octokit, context, ref, 123, null, true)).toStrictEqual({"branch": "octocats-everywhere", "created_at": "2022-06-14T21:12:14.041Z", "created_by": "octocat", "link": "https://github.com/test-org/test-repo/pull/2#issuecomment-456", "reason": "Testing my new feature with lots of cats", "sticky": true})
+})
+
+test('Request detailsOnly on the lock file when the lock branch exists but no lock file exists', async () => {
+  const octokit = {
+    rest: {
+      repos: {
+        getBranch: jest
+          .fn()
+          .mockReturnValueOnce({data: {commit: {sha: 'abc123'}}}),
+        get: jest.fn().mockReturnValue({data: {default_branch: 'main'}}),
+        getContent: jest
+          .fn()
+          .mockRejectedValue(new NotFoundError('file not found')),
+        createOrUpdateFileContents: jest.fn().mockReturnValue({})
+      },
+      issues: {
+        createComment: jest.fn().mockReturnValue({})
+      }
+    }
+  }
+  expect(await lock(octokit, context, ref, 123, null, true)).toBe(null)
+})
+
+test('Request detailsOnly on the lock file when no branch exists', async () => {
+  const context = {
+    actor: 'monalisa',
+    repo: {
+      owner: 'corp',
+      repo: 'test'
+    },
+    issue: {
+      number: 1
+    },
+    payload: {
+      comment: {
+        body: '.lock --details',
+        id: 123
+      }
+    }
+  }
+  const octokit = {
+    rest: {
+      repos: {
+        getBranch: jest
+          .fn()
+          .mockRejectedValueOnce(new NotFoundError('Reference does not exist'))
+          .mockReturnValueOnce({data: {commit: {sha: 'abc123'}}}),
+        get: jest.fn().mockReturnValue({data: {default_branch: 'main'}}),
+        createOrUpdateFileContents: jest.fn().mockReturnValue({})
+      },
+      git: {
+        createRef: jest.fn().mockReturnValue({status: 201})
+      },
+      issues: {
+        createComment: jest.fn().mockReturnValue({})
+      }
+    }
+  }
+  expect(await lock(octokit, context, ref, 123, null, true)).toBe(null)
+})
+
 test('Determines that the lock request is coming from current owner of the lock and exits - non-sticky', async () => {
   const octokit = {
     rest: {
