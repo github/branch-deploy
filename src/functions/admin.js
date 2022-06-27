d1eb88df2de05fcd1318990a895ecad2c20c6c2f
Grant Birkinbine <grantbirki@github.com>
2022-06-26 19:28:16 -0600

    admin check code

--- src/functions/admin.js
+++ src/functions/admin.js
@@ -1,64 +1,114 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import githubUsernameRegex from 'github-username-regex';
+import githubUsernameRegex from 'github-username-regex'
 
+// Helper function to check if a user exists in an org team
+// :param actor: The user to check
+// :param orgTeams: An array of org/team names
+// :returns: True if the user is in the org team, false otherwise
 async function orgCheck(actor, orgTeams) {
-    // Create a new octokit client with the admin PAT
-    // This pat needs org read permissions if you are using org/teams to define admins
-    const adminsPat = core.getInput('admins_pat')
+  // This pat needs org read permissions if you are using org/teams to define admins
+  const adminsPat = core.getInput('admins_pat')
 
-    // If no admin_pat is provided, then we cannot check for org team memberships
-    if (!adminsPat || adminsPat.length === 0) {
-        core.warning('No admins_pat provided, skipping admin check for org team membership')
-        return false
+  // If no admin_pat is provided, then we cannot check for org team memberships
+  if (!adminsPat || adminsPat.length === 0) {
+    core.warning(
+      'No admins_pat provided, skipping admin check for org team membership'
+    )
+    return false
+  }
+
+  // Create a new octokit client with the admins_pat
+  const octokit = github.getOctokit(adminsPat)
+
+  // Loop through all org/team names
+  orgTeams.forEach(async orgTeam => {
+    // Split the org/team name into org and team
+    var [org, team] = orgTeam.split('/')
+
+    try {
+      // Make an API call to get the org id
+      const orgData = await octokit.rest.orgs.get({
+        org: org
+      })
+      const orgId = orgData.data.id
+
+      // Make an API call to get the team id
+      const teamData = await octokit.rest.teams.getByName({
+        org: org,
+        team_slug: team
+      })
+      const teamId = teamData.data.id
+
+      // This API call checks if the user exists in the team for the given org
+      const result = await octokit.request(
+        `GET /organizations/${orgId}/team/${teamId}/members/${actor}`
+      )
+      // If the status code is a 204, the user is in the team
+      if (result.status === 204) {
+        core.debug(`${actor} is in ${orgTeam}`)
+        return true
+        // If some other status code occured, return false and output a warning
+      } else {
+        core.warning(`non 204 response from org team check: ${result.status}`)
+      }
+    } catch (error) {
+      // If any of the API calls returns a 404, the user is not in the team
+      if (error.status === 404) {
+        core.debug(`${actor} is not a member of the ${orgTeam} team`)
+        // If some other error occured, output a warning
+      } else {
+        core.warning(`Error checking org team membership: ${error}`)
+      }
     }
+  })
 
-    const octokit = github.getOctokit(adminsPat)
+  // If we get here, the user is not in any of the org teams
+  return false
 }
-
 
 // Helper function to check if a user is set as an admin for branch-deployments
 // :param context: The GitHub Actions event context
 // :returns: true if the user is an admin, false otherwise (Boolean)
-export async function isAdmin(
-    context
-) {
-    // Get the admins string from the action inputs
-    const admins = core.getInput('admins')
+export async function isAdmin(context) {
+  // Get the admins string from the action inputs
+  const admins = core.getInput('admins')
 
-    // Sanitized the input to remove any whitespace and split into an array
-    const adminsSanitized = admins.split(",").map(admin => admin.trim())
+  // Sanitized the input to remove any whitespace and split into an array
+  const adminsSanitized = admins.split(',').map(admin => admin.trim())
 
-    // loop through admins
-    var handles = []
-    var orgTeams = []
-    adminsSanitized.forEach(admin => {
-        // If the item contains a '/', then it is a org/team
-        if (admin.includes("/")) {
-            orgTeams.push(admin)
-        }
-        // Otherwise, it is a github handle
-        else {
-            // Check if the github handle is valid
-            if (githubUsernameRegex.test(admin)) {
-                // Add the handle to the list of handles and remove @ from the start of the handle
-                handles.push(admin.replace('@', ''))
-            }
-            else {
-                console.log(`${admin} is not a valid GitHub username... skipping`)
-            }
-        }
-    })
-
-    // Check if the user is in the admin handle list
-    if (handles.includes(context.actor)) {
-        core.debug(`${context.actor} is an admin via handle reference`)
-        return true
-    } else if (orgTeams.length > 0 && await orgCheck(context.actor, orgTeams) === true) {
-        core.debug(`${context.actor} is an admin via org team reference`)
-        return true
+  // loop through admins
+  var handles = []
+  var orgTeams = []
+  adminsSanitized.forEach(admin => {
+    // If the item contains a '/', then it is a org/team
+    if (admin.includes('/')) {
+      orgTeams.push(admin)
     }
+    // Otherwise, it is a github handle
+    else {
+      // Check if the github handle is valid
+      if (githubUsernameRegex.test(admin)) {
+        // Add the handle to the list of handles and remove @ from the start of the handle
+        handles.push(admin.replace('@', ''))
+      } else {
+        console.log(`${admin} is not a valid GitHub username... skipping`)
+      }
+    }
+  })
 
-    // If we get here, the user is not an admin
-    return false
+  // Check if the user is in the admin handle list
+  if (handles.includes(context.actor)) {
+    core.debug(`${context.actor} is an admin via handle reference`)
+    return true
+  } else if (
+    orgTeams.length > 0 &&
+    (await orgCheck(context.actor, orgTeams)) === true
+  ) {
+    core.debug(`${context.actor} is an admin via org team reference`)
+    return true
+  }
+
+  // If we get here, the user is not an admin
+  return false
 }
