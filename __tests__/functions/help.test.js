8bb63a4f49a86ee705116e9525e02773254b3459
GrantBirki <grantbirki@github.com>
2023-02-15 14:21:05 +0000

    fully test .help

--- __tests__/functions/help.test.js
+++ __tests__/functions/help.test.js
@@ -421,4 +421,112 @@ test('successfully calls help with non-defaults', async () => {
   `)
 
   expect(debugMock).toHaveBeenCalledWith(comment)
+
+
+  var inputsSecond = inputs
+  inputsSecond.update_branch = 'disabled'
+  expect(await help(octokit, context, 123, inputsSecond))
+
+  const commentSecond = dedent(`
+  ## 📚 Branch Deployment Help
+
+  This help message was automatically generated based on the inputs provided to this Action.
+
+  ### 💻 Available Commands
+
+  - \`${inputs.help_trigger}\` - Show this help message
+  - \`${inputs.trigger}\` - Deploy this branch to the \`${
+    inputs.environment
+  }\` environment
+  - \`${inputs.trigger} ${inputs.stable_branch}\` - Rollback the \`${
+    inputs.environment
+  }\` environment to the \`${inputs.stable_branch}\` branch
+  - \`${inputs.trigger} ${
+    inputs.noop_trigger
+  }\` - Deploy this branch to the \`${
+    inputs.environment
+  }\` environment in noop mode
+  - \`${
+    inputs.lock_trigger
+  }\` - Obtain the deployment lock (will persist until the lock is released)
+  - \`${
+    inputs.lock_trigger
+  } --reason <text>\` - Obtain the deployment lock with a reason (will persist until the lock is released)
+  - \`${inputs.unlock_trigger}\` - Release the deployment lock (if one exists)
+  - \`${
+    inputs.lock_trigger
+  } --info\` - Show information about the current deployment lock (if one exists)
+  - \`${inputs.lock_info_alias}\` - Alias for \`${inputs.lock_trigger} --info\`
+
+  ### 🌍 Environments
+
+  These are the available environments for this Action as defined by the inputs provided to this Action.
+
+  > Note: Just because an environment is listed here does not mean it is available for deployment
+
+  - \`${inputs.environment}\` - The default environment for this Action
+  - \`${
+    inputs.production_environment
+  }\` - The environment that is considered "production"
+  - \`${
+    inputs.environment_targets
+  }\` - The list of environments that can be targeted for deployment
+
+  ### 🔭 Example Commands
+
+  The following set of examples use this Action's inputs to show you how to use the commands.
+
+  - \`${inputs.trigger}\` - Deploy the \`${
+    context.payload.pull_request.head.ref
+  }\` branch to the \`${inputs.environment}\` environment
+  - \`${inputs.trigger} ${inputs.stable_branch}\` - Rollback the \`${
+    inputs.environment
+  }\` environment to the \`${inputs.stable_branch}\` branch
+  - \`${inputs.trigger} ${inputs.noop_trigger}\` - Deploy the \`${
+    context.payload.pull_request.head.ref
+  }\` branch to the \`${inputs.environment}\` environment in noop mode
+  - \`${inputs.trigger} to <${inputs.environment_targets.replace(
+    ',',
+    '|'
+  )}>\` - Deploy the \`${
+    context.payload.pull_request.head.ref
+  }\` branch to the specified environment (note: the \`to\` keyword is optional)
+
+  ### ⚙️ Configuration
+
+  The following configuration options have been defined for this Action:
+
+  - \`reaction: ${
+    inputs.reaction
+  }\` - The GitHub reaction icon to add to the deployment comment when a deployment is triggered
+  - \`update_branch: ${
+    inputs.update_branch
+  }\` - This Action will not update the branch to the base branch before deployment
+  - \`required_contexts: ${
+    inputs.required_contexts
+  }\` - There are required contexts designated for this Action
+  - \`allowForks: ${inputs.allowForks}\` - This Action will ${
+    inputs.allowForks === 'true' ? 'run' : 'not run'
+  } on forked repositories
+  - \`prefixOnly: ${inputs.prefixOnly}\` - This Action will ${
+    inputs.prefixOnly === 'true'
+      ? 'only run if the comment starts with the trigger'
+      : 'run if the comment contains the trigger anywhere in the comment body'
+  }
+  - \`skipCi: ${
+    inputs.skipCi
+  }\` - This Action will not require passing CI for the environments specified
+  - \`skipReviews: ${
+    inputs.skipReviews
+  }\` - This Action will not require passing reviews for the environments specified
+  - \`admins: ${
+    inputs.admins
+  }\` - This Action will allow the listed admins to bypass pull request reviews before deployment
+
+  ---
+
+  > View the full usage guide [here](https://github.com/github/branch-deploy/blob/main/docs/usage.md) for additional help
+  `)
+
+  expect(debugMock).toHaveBeenCalledWith(commentSecond)
 })
