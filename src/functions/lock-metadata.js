6f0fc0db46ee308ffdece025e4bb583e547093ad
GrantBirki <grantbirki@github.com>
2023-08-16 23:02:15 +0100

    leading space only

--- src/functions/lock-metadata.js
+++ src/functions/lock-metadata.js
@@ -1,5 +1,5 @@
 export const LOCK_METADATA = {
-  lockInfoFlags: [' --info ', ' --i ', ' -i ', ' --details ', ' --d ', ' -d '],
+  lockInfoFlags: [' --info', ' --i', ' -i', ' --details', ' --d', ' -d'],
   lockBranchSuffix: 'branch-deploy-lock',
   globalLockBranch: 'global-branch-deploy-lock',
   lockCommitMsg: 'lock [skip ci]',
