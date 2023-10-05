b7d81502c7bfd57b7905ece82b60d5550369f041
GrantBirki <grantbirki@github.com>
2023-10-04 18:05:53 -0600

    bundle

--- dist/index.js
+++ dist/index.js
@@ -20090,7 +20090,6 @@ async function prechecks(
   const skipReviews = skipReviewsArray.includes(environment)
   const allowDraftDeploy = draftPermittedTargetsArray.includes(environment)
 
-  // check if comment starts with the env.DEPLOY_COMMAND variable followed by the 'main' branch or if this is for the current branch
   var ref = pr.data.head.ref
   var noopMode = environmentObj.noop
   var forkBypass = false
