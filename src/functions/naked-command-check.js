0398514e8cca1ff8fb0c564e0bfe9b4a724f8db5
GrantBirki <grantbirki@github.com>
2023-10-05 15:44:48 -0600

    init helper function

---
+++ src/functions/naked-command-check.js
@@ -0,0 +1,5 @@
+// Helper function to check if a naked command was issued
+// :param body: The body of the issueops command
+// :param triggers: All the triggers for the Action rolled up into a hash
+// :returns: true if a naked command was issued, false otherwise
+export async function nakedCommandCheck(body, triggers) {}
