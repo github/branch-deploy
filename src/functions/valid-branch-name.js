eaf928587a545500fbf58ec672a6ff3d8ed0df30
GrantBirki <grant.birkinbine@gmail.com>
2024-08-31 10:22:47 -0700

    adding debug logs

--- src/functions/valid-branch-name.js
+++ src/functions/valid-branch-name.js
@@ -1,7 +1,11 @@
+import * as core from '@actions/core'
+
 // Helper function to create a valid branch name that will pass GitHub's API ref validation
 // :param branch: The branch name
 // :returns: A string of the branch name with proper formatting
 export function constructValidBranchName(branch) {
+  core.debug(`constructing valid branch name: ${branch}`)
+
   if (branch === null) {
     return null
   } else if (branch === undefined) {
@@ -11,5 +15,6 @@ export function constructValidBranchName(branch) {
   // If environment contains any spaces, replace all of them with a hyphen
   branch = branch.replace(/\s/g, '-')
 
+  core.debug(`constructed valid branch name: ${branch}`)
   return branch
 }
