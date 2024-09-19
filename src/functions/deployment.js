7bca69c3f362ff5e792d0f96b506a7bf7cf87c56
GrantBirki <grant.birkinbine@gmail.com>
2024-09-19 13:45:02 -0700

    add a helper method for fetching the latest deployment for a given env

--- src/functions/deployment.js
+++ src/functions/deployment.js
@@ -32,3 +32,68 @@ export async function createDeploymentStatus(
 
   return result
 }
+
+// Helper function to get the latest deployment for a given environment
+// :param octokit: The octokit client
+// :param context: The GitHub Actions event context
+// :param environment: The environment to get the latest deployment for (ex: production)
+// :returns: The result of the deployment (Object)
+export async function latestDeployment(octokit, context, environment) {
+  // Get the owner and the repo from the context
+  const {owner, repo} = context.repo
+
+  const query = `
+    query ($repo_owner: String!, $repo_name: String!, $environment: String!) {
+      repository(owner: $repo_owner, name: $repo_name) {
+        deployments(environments: [$environment], first: 1, orderBy: { field: CREATED_AT, direction: DESC }) {
+          nodes {
+            createdAt
+            environment
+            updatedAt
+            state
+            ref {
+              name
+            }
+            commit {
+              oid
+            }
+          }
+        }
+      }
+    }`
+
+  const variables = {
+    repo_owner: owner,
+    repo_name: repo,
+    environment: environment
+  }
+
+  const data = await octokit.graphql(query, variables)
+  const nodes = data.repository.deployments.nodes
+
+  // nodes may be empty if no matching deployments were found - ex: []
+  // otherwise, nodes may look like this:
+  // [
+  //   {
+  //       "createdAt": "2024-09-19T20:18:18Z",
+  //       "environment": "production",
+  //       "updatedAt": "2024-09-19T20:18:21Z",
+  //       "state": "ACTIVE",
+  //       "ref": {
+  //           "name": "main"
+  //       },
+  //       "commit": {
+  //           "oid": "315cec138fc9d7dac8a47c6bba4217d3965ede3b"
+  //       }
+  //   }
+  // ]
+
+  // If no deployments were found, return null
+  if (nodes.length === 0) {
+    return null
+  }
+
+  // Otherwise, return the latest deployment
+  return nodes[0]
+}
+
