a3f52cd150e88698873554abc17f2884e8a3d155
GrantBirki <grant.birkinbine@gmail.com>
2024-11-22 11:31:27 -0800

    add more unit tests

--- __tests__/functions/params.test.js
+++ __tests__/functions/params.test.js
@@ -14,3 +14,33 @@ test('it parses arguments using the default settings of library', async () => {
 test('it works with empty string', async () => {
   expect(parseParams('')).toHaveProperty('_', [])
 })
+
+test('it parses multiple positional parameters', async () => {
+  expect(parseParams('foo bar baz')).toHaveProperty('_', ['foo', 'bar', 'baz'])
+})
+
+test('it parses flags correctly', async () => {
+  const parsed = parseParams('--foo --bar')
+  expect(parsed).toHaveProperty('foo', true)
+  expect(parsed).toHaveProperty('bar', true)
+  expect(parsed).toHaveProperty('_', [])
+})
+
+test('it parses numeric values correctly', async () => {
+  const parsed = parseParams('--count 42')
+  expect(parsed).toHaveProperty('count', 42)
+  expect(parsed).toHaveProperty('_', [])
+})
+
+test('it parses boolean values correctly', async () => {
+  const parsed = parseParams('--enabled=true --disabled false')
+  expect(parsed).toHaveProperty('enabled', 'true')
+  expect(parsed).toHaveProperty('disabled', 'false')
+  expect(parsed).toHaveProperty('_', [])
+})
+
+test('it parses nested objects correctly', async () => {
+  const parsed = parseParams('--config.db.host=localhost --config.db.port=5432')
+  expect(parsed).toHaveProperty('config', {db: {host: 'localhost', port: 5432}})
+  expect(parsed).toHaveProperty('_', [])
+})
