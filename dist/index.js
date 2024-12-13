1e10a988167e72dbcfc9d5cc369880cc480e8be9
GrantBirki <grant.birkinbine@gmail.com>
2024-12-13 11:58:36 -0800

    keep the same logic in `branchRulesetChecks()` but refactor it so that it is cleaner

--- dist/index.js
+++ dist/index.js
@@ -43456,72 +43456,113 @@ const SUGGESTED_RULESETS = [
 
 async function branchRulesetChecks(context, octokit, data) {
   const branch = data.branch
-  const use_security_warnings = data?.use_security_warnings !== false
+  const useSecurityWarnings = data?.use_security_warnings !== false
 
-  // exit early if the user has disabled security warnings
-  if (!use_security_warnings) {
+  // Exit early if the user has disabled security warnings
+  if (!useSecurityWarnings) {
     return {success: true}
   }
 
-  const {data: branch_rules} = await octokit.rest.repos.getBranchRules({
+  const {data: branchRules} = await octokit.rest.repos.getBranchRules({
     ...context.repo,
     branch,
     headers: API_HEADERS
   })
 
   core.debug(
-    `branch ${COLORS.highlight}rulesets${COLORS.reset}: ${JSON.stringify(branch_rules)}`
+    `branch ${COLORS.highlight}rulesets${COLORS.reset}: ${JSON.stringify(branchRules)}`
   )
 
-  var failed_checks = []
+  const failed_checks = []
 
-  // leave a warning if no rulesets are defined
-  if (branch_rules.length === 0) {
+  // Leave a warning if no rulesets are defined
+  if (branchRules.length === 0) {
     core.warning(
       `🔐 branch ${COLORS.highlight}rulesets${COLORS.reset} are not defined for branch ${COLORS.highlight}${branch}${COLORS.reset}`
     )
     failed_checks.push('missing_branch_rulesets')
   } else {
-    // loop through the suggested rulesets and check them against the branch rules
-    SUGGESTED_RULESETS.forEach(suggested_rule => {
-      const rule_type = suggested_rule.type
-      const rule_parameters = suggested_rule.parameters
+    // Loop through the suggested rulesets and check them against the branch rules
+    SUGGESTED_RULESETS.forEach(suggestedRule => {
+      const {type: ruleType, parameters: ruleParameters} = suggestedRule
 
-      const branch_rule = branch_rules.find(rule => rule.type === rule_type)
+      const branchRule = branchRules.find(rule => rule.type === ruleType)
 
-      if (!branch_rule) {
-        core.warning(
-          `🔐 branch ${COLORS.highlight}rulesets${COLORS.reset} for branch ${COLORS.highlight}${branch}${COLORS.reset} is missing a rule of type ${COLORS.highlight}${rule_type}${COLORS.reset}`
+      if (!branchRule) {
+        logMissingRule(branch, ruleType, failed_checks)
+      } else if (ruleParameters) {
+        checkRuleParameters(
+          branch,
+          ruleType,
+          ruleParameters,
+          branchRule,
+          failed_checks
         )
-        failed_checks.push(`missing_${rule_type}`)
-      } else if (rule_parameters) {
-        Object.keys(rule_parameters).forEach(key => {
-          if (branch_rule.parameters[key] !== rule_parameters[key]) {
-            if (key === 'required_approving_review_count') {
-              if (
-                branch_rule.parameters['required_approving_review_count'] === 0
-              ) {
-                core.warning(
-                  `🔐 branch ${COLORS.highlight}rulesets${COLORS.reset} for branch ${COLORS.highlight}${branch}${COLORS.reset} contains the required_approving_review_count parameter but it is set to 0`
-                )
-                failed_checks.push(`mismatch_${rule_type}_${key}`)
-              } else {
-                core.debug(
-                  `required_approving_review_count is ${branch_rule.parameters['required_approving_review_count']} - OK`
-                )
-              }
-            } else {
-              core.warning(
-                `🔐 branch ${COLORS.highlight}rulesets${COLORS.reset} for branch ${COLORS.highlight}${branch}${COLORS.reset} contains a rule of type ${COLORS.highlight}${rule_type}${COLORS.reset} with a parameter ${COLORS.highlight}${key}${COLORS.reset} which does not match the suggested parameter`
-              )
-              failed_checks.push(`mismatch_${rule_type}_${key}`)
-            }
-          }
-        })
       }
     })
   }
 
+  logWarnings(failed_checks)
+
+  // If there are no failed checks, log a success message
+  if (failed_checks.length === 0) {
+    core.info(`🔐 branch ruleset checks ${COLORS.success}passed${COLORS.reset}`)
+  }
+
+  return {success: failed_checks.length === 0, failed_checks}
+}
+
+function logMissingRule(branch, ruleType, failed_checks) {
+  core.warning(
+    `🔐 branch ${COLORS.highlight}rulesets${COLORS.reset} for branch ${COLORS.highlight}${branch}${COLORS.reset} is missing a rule of type ${COLORS.highlight}${ruleType}${COLORS.reset}`
+  )
+  failed_checks.push(`missing_${ruleType}`)
+}
+
+function checkRuleParameters(
+  branch,
+  ruleType,
+  ruleParameters,
+  branchRule,
+  failed_checks
+) {
+  Object.keys(ruleParameters).forEach(key => {
+    if (branchRule.parameters[key] !== ruleParameters[key]) {
+      if (key === 'required_approving_review_count') {
+        handleReviewCountMismatch(branch, ruleType, branchRule, failed_checks)
+      } else {
+        logParameterMismatch(branch, ruleType, key, failed_checks)
+      }
+    }
+  })
+}
+
+function handleReviewCountMismatch(
+  branch,
+  ruleType,
+  branchRule,
+  failed_checks
+) {
+  if (branchRule.parameters['required_approving_review_count'] === 0) {
+    core.warning(
+      `🔐 branch ${COLORS.highlight}rulesets${COLORS.reset} for branch ${COLORS.highlight}${branch}${COLORS.reset} contains the required_approving_review_count parameter but it is set to 0`
+    )
+    failed_checks.push(`mismatch_${ruleType}_required_approving_review_count`)
+  } else {
+    core.debug(
+      `required_approving_review_count is ${branchRule.parameters['required_approving_review_count']} - OK`
+    )
+  }
+}
+
+function logParameterMismatch(branch, ruleType, key, failed_checks) {
+  core.warning(
+    `🔐 branch ${COLORS.highlight}rulesets${COLORS.reset} for branch ${COLORS.highlight}${branch}${COLORS.reset} contains a rule of type ${COLORS.highlight}${ruleType}${COLORS.reset} with a parameter ${COLORS.highlight}${key}${COLORS.reset} which does not match the suggested parameter`
+  )
+  failed_checks.push(`mismatch_${ruleType}_${key}`)
+}
+
+function logWarnings(failed_checks) {
   if (failed_checks.length > 0) {
     core.warning(
       `😨 the following branch ruleset warnings were detected: ${failed_checks.join(', ')}`
@@ -43530,13 +43571,6 @@ async function branchRulesetChecks(context, octokit, data) {
       `📚 your branch ruleset settings may be insecure - please review the documentation: https://github.com/github/branch-deploy/blob/main/docs/branch-rulesets.md`
     )
   }
-
-  // if there are no failed checks, log a success message
-  if (failed_checks.length === 0) {
-    core.info(`🔐 branch ruleset checks ${COLORS.success}passed${COLORS.reset}`)
-  }
-
-  return {success: failed_checks.length === 0, failed_checks: failed_checks}
 }
 
 ;// CONCATENATED MODULE: ./src/functions/valid-branch-name.js
