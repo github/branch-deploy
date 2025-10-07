efc7fbf44f5ae6b39a45e93c34cb5391c1f1af3b
GrantBirki <grant.birkinbine@gmail.com>
2025-10-07 13:35:37 -0700

    fix version tests

--- __tests__/version.test.js
+++ __tests__/version.test.js
@@ -1,35 +1,34 @@
 import {VERSION} from '../src/version.js'
-import {vi, expect, describe, test, beforeEach} from 'vitest'
+import {expect, test} from 'vitest'
 
-describe('VERSION constant', () => {
-  const versionRegex = /^v(\d+)\.(\d+)\.(\d+)(?:-rc\.(\d+))?$/
+const versionRegex = /^v(\d+)\.(\d+)\.(\d+)(?:-rc\.(\d+))?$/
 
-  it('should match the version pattern', () => {
-    expect(VERSION).toMatch(versionRegex)
-  })
-
-  it('should validate v1.0.0', () => {
-    const version = 'v1.0.0'
-    expect(version).toMatch(versionRegex)
-  })
-
-  it('should validate v4.5.1', () => {
-    const version = 'v4.5.1'
-    expect(version).toMatch(versionRegex)
-  })
-
-  it('should validate v10.123.44', () => {
-    const version = 'v10.123.44'
-    expect(version).toMatch(versionRegex)
-  })
-
-  it('should validate v1.1.1-rc.1', () => {
-    const version = 'v1.1.1-rc.1'
-    expect(version).toMatch(versionRegex)
-  })
-
-  it('should validate v15.19.4-rc.35', () => {
-    const version = 'v15.19.4-rc.35'
-    expect(version).toMatch(versionRegex)
-  })
+test('VERSION constant should match the version pattern', () => {
+  expect(VERSION).toMatch(versionRegex)
 })
+
+test('should validate v1.0.0', () => {
+  const version = 'v1.0.0'
+  expect(version).toMatch(versionRegex)
+})
+
+test('should validate v4.5.1', () => {
+  const version = 'v4.5.1'
+  expect(version).toMatch(versionRegex)
+})
+
+test('should validate v10.123.44', () => {
+  const version = 'v10.123.44'
+  expect(version).toMatch(versionRegex)
+})
+
+test('should validate v1.1.1-rc.1', () => {
+  const version = 'v1.1.1-rc.1'
+  expect(version).toMatch(versionRegex)
+})
+
+test('should validate v15.19.4-rc.35', () => {
+  const version = 'v15.19.4-rc.35'
+  expect(version).toMatch(versionRegex)
+})
+
